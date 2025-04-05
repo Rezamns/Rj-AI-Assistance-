533c0b02a4192443c9c5e1631285ed7fd9499f48
RM Coder🦉 <57560653+Rezamns@users.noreply.github.com>
2025-04-05 22:41:25 +0330

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ app.post("/api/chat", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("سرور روی http://localhost:3000 فعال شد");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`سرور روی پورت ${PORT} فعال شد`);
 });
